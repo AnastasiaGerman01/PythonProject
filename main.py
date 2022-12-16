b5a54d47a485e2f2135fbb925f68756fee61853d
AnastasiaGerman01 <86071682+AnastasiaGerman01@users.noreply.github.com>
2022-12-16 20:08:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import telebot
 import aiogram
 from aiogram import Bot, Dispatcher, executor, utils, types
 from aiogram.types import ParseMode, ReplyKeyboardMarkup, KeyboardButton
-from db import process_search_model, init_db, find_id_search, find_all_laptops
+from db import process_search_model, init_db, find_id_search, find_all_laptops, delete_last_message
 from parser_ import ParseLaptop
 
 from config import TOKEN, URL, XPATH
@@ -18,13 +18,25 @@ dp = Dispatcher(bot)
 
 #Создание клавиатуры
 b1 = KeyboardButton('/search')
-keyboard = aiogram.types.ReplyKeyboardMarkup(resize_keyboard=True)
-keyboard.add(b1)
+b2 = KeyboardButton('/requests')
+b3 = KeyboardButton('/add')
+b4 = KeyboardButton('/help')
+keyboard1 = aiogram.types.ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard1.add(b1, b2)
+keyboard1.add(b3, b4)
+
+b5 = KeyboardButton('/return')
+b6 = KeyboardButton('/delete')
+keyboard2 = aiogram.types.ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard2.add(b5)
+keyboard2.add(b6)
+
+help_message = "Hi! \n In this telegram bot you can use the following commands: \n \help \n"
 
 #Начало работы с telegram bot
 @dp.message_handler(commands = 'start')
 async def start(message: types.Message):
-    await bot.send_message(message.from_user.id, "Hello!", reply_markup=keyboard)
+    await bot.send_message(message.from_user.id, "Hello!", reply_markup=keyboard1)
 
 #Функция обработки ключевых слов поиска
 @dp.message_handler(commands = 'requests')
@@ -41,8 +53,27 @@ async def scheduled (message: types.Message):
         laptop.delete_instance()
     await parser.parse()
 
+#Функция добавления новых слов
+@dp.message_handler(commands = 'add')
+async def scheduled (message: types.Message):
+    await bot.send_message(message.from_user.id, "Type new words", reply_markup=keyboard2)
 
-#Функция, возвращающая все введённые пользователем ключевые слова
+#Функция удаления последнего слова
+@dp.message_handler(commands = 'delete')
+async def scheduled (message: types.Message):
+    await delete_last_message(message)
+
+#Функция удаления последнего слова
+@dp.message_handler(commands = 'help')
+async def scheduled (message: types.Message):
+    await bot.send_message(message.from_user.id,help_message, reply_markup=keyboard1)
+
+#Функция возврата главного меню
+@dp.message_handler(commands = 'return')
+async def scheduled (message: types.Message):
+    await bot.send_message(message.from_user.id, "Now you can search!", reply_markup=keyboard1)
+
+#Функция, обрабатывающая введённые пользователем слова
 @dp.message_handler()
 async def echo(message: types.Message):
     if message.text != '/start' and message.text != '/search':
@@ -54,4 +85,3 @@ if __name__ =='__main__':
     init_db()
     parser = ParseLaptop(url=URL,xpath=XPATH, bot = bot)
     executor.start_polling(dp, skip_updates=True)
-
