2178b6b787576543ddf6628570f95952339a2531
AnastasiaGerman01 <86071682+AnastasiaGerman01@users.noreply.github.com>
2022-12-17 10:54:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
 URL = 'https://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/?p={}'
 XPATH = '/html/body/div[1]/div/div[2]/div/div[3]/div/div[2]/div[{}]'
-TOKEN = '5822692394:AAEFxy0UJAhUueLFSkomYg2H2udtS1W3AvI'
 DRIVER_PATH = r"C:\Users\user\PycharmProjects\Bot\chromedriver.exe"
